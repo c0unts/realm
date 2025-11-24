f405d6b6afec034a4cddb97f779253714333ade4
Lukas Peters <peterslukas0512@gmail.com>
2025-11-23 23:59:12 -0500

    Implement actual code

--- implants/lib/eldritch/src/sys/get_users_impl.rs
+++ implants/lib/eldritch/src/sys/get_users_impl.rs
@@ -1,9 +1,28 @@
-pub fn get_users() -> Result<Dict> {} {
-    use sysinfo::Users;
+use super::super::insert_dict_kv;
+use anyhow::Result;
+use starlark::collections::SmallMap;
+use starlark::values::dict::Dict;
+use starlark::values::Heap;
+use sysinfo::{System, SystemExt, UserExt};
 
-    let mut users = Users::new();
-    users.refresh();
-    for user in users.list() {
-        println!("{user:?}");
+pub fn get_users(starlark_heap: &Heap) -> Result<Dict> {
+    let res = SmallMap::new();
+    let mut dict_res = Dict::new(res);
+
+    let sys = System::new_all();
+    for user in sys.users() {
+        insert_dict_kv!(dict_res, starlark_heap, "id", **user.id(), u32);
+        insert_dict_kv!(dict_res, starlark_heap, "name", user.name(), String);
+        insert_dict_kv!(dict_res, starlark_heap, "gid", *user.group_id(), u32);
+        insert_dict_kv!(
+            dict_res,
+            starlark_heap,
+            "groups",
+            Vec::from(user.groups()),
+            Vec<_>
+        );
+        Ok(());
     }
+
+    return Ok(dict_res);
 }
