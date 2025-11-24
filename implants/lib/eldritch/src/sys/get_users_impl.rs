071e9b4717d0c095d4925a6f572c30a88bfc2f23
Lukas Peters <peterslukas0512@gmail.com>
2025-11-24 00:54:45 -0500

    Lifetime rust??

--- implants/lib/eldritch/src/sys/get_users_impl.rs
+++ implants/lib/eldritch/src/sys/get_users_impl.rs
@@ -1,28 +1,37 @@
 use super::super::insert_dict_kv;
 use anyhow::Result;
-use starlark::collections::SmallMap;
-use starlark::const_frozen_string;
-use starlark::values::dict::Dict;
-use starlark::values::Heap;
-use sysinfo::{System, SystemExt, UserExt};
+use starlark::{
+    collections::SmallMap,
+    const_frozen_string,
+    values::{dict::Dict, Heap, Value},
+};
+use sysinfo::{System, SystemExt, User, UserExt};
 
-pub fn get_users(starlark_heap: &Heap) -> Result<Dict> {
-    let res = SmallMap::new();
-    let mut dict_res = Dict::new(res);
+fn create_dict_from_users<'a>(starlark_heap: &'a Heap, user: &User) -> Result<Dict<'a>> {
+    let res: SmallMap<Value, Value> = SmallMap::new();
+    let mut tmp_res: Dict<'_> = Dict::new(res);
+
+    insert_dict_kv!(tmp_res, starlark_heap, "id", **user.id(), u32);
+    insert_dict_kv!(tmp_res, starlark_heap, "name", user.name(), String);
+    insert_dict_kv!(tmp_res, starlark_heap, "gid", *user.group_id(), u32);
+    insert_dict_kv!(
+        tmp_res,
+        starlark_heap,
+        "groups",
+        Vec::from(user.groups()),
+        Vec<_>
+    );
+
+    Ok(tmp_res)
+}
+
+pub fn get_users(starlark_heap: &Heap) -> Result<Vec<Dict>> {
+    let mut final_res: Vec<Dict> = Vec::new();
 
     let sys = System::new_all();
     for user in sys.users() {
-        insert_dict_kv!(dict_res, starlark_heap, "id", **user.id(), u32);
-        insert_dict_kv!(dict_res, starlark_heap, "name", user.name(), String);
-        insert_dict_kv!(dict_res, starlark_heap, "gid", *user.group_id(), u32);
-        insert_dict_kv!(
-            dict_res,
-            starlark_heap,
-            "groups",
-            Vec::from(user.groups()),
-            Vec<_>
-        );
+        let tmp_res: Dict<'_> = create_dict_from_users(starlark_heap, user)?;
+        final_res.push(tmp_res);
     }
-
-    Ok(dict_res)
+    Ok(final_res)
 }
