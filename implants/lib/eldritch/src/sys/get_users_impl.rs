5bca7c80f56a1fb18bf7ee673b9f635899b078fa
Lukas Peters <peterslukas0512@gmail.com>
2025-11-24 21:52:04 -0500

    Add testing

--- implants/lib/eldritch/src/sys/get_users_impl.rs
+++ implants/lib/eldritch/src/sys/get_users_impl.rs
@@ -11,7 +11,12 @@ fn create_dict_from_users<'a>(starlark_heap: &'a Heap, user: &User) -> Result<Di
     let res: SmallMap<Value, Value> = SmallMap::new();
     let mut tmp_res: Dict<'_> = Dict::new(res);
 
+    #[cfg(target_os = "windows")]
+    insert_dict_kv!(tmp_res, starlark_heap, "id", &*user.id().to_string(), u32);
+
+    #[cfg(not(target_os = "windows"))]
     insert_dict_kv!(tmp_res, starlark_heap, "id", **user.id(), u32);
+
     insert_dict_kv!(tmp_res, starlark_heap, "name", user.name(), String);
     insert_dict_kv!(tmp_res, starlark_heap, "gid", *user.group_id(), u32);
     insert_dict_kv!(
@@ -33,5 +38,35 @@ pub fn get_users(starlark_heap: &Heap) -> Result<Vec<Dict>> {
         let tmp_res: Dict<'_> = create_dict_from_users(starlark_heap, user)?;
         final_res.push(tmp_res);
     }
+
     Ok(final_res)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use anyhow::Ok;
+
+    #[test]
+    fn test_sys_get_users() -> anyhow::Result<()> {
+        let test_heap = Heap::new();
+        let res: Vec<Dict<'_>> = get_users(&test_heap)?;
+
+        // make sure it is not empty
+        assert!(!res.is_empty());
+
+        // using the first dict for testing
+        let keys: Vec<&str> = res
+            .first()
+            .unwrap()
+            .keys()
+            .map(|key| key.unpack_str().unwrap())
+            .collect();
+        assert!(keys.contains(&"id"));
+        assert!(keys.contains(&"name"));
+        assert!(keys.contains(&"gid"));
+        assert!(keys.contains(&"groups"));
+
+        Ok(())
+    }
 }
